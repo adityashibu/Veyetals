bb87e9fb3c3ff69d7553e8efc367a31537806537
Aditya Shibu <as2397@hw.ac.uk>
2024-08-25 19:53:23 +0400

    Added base data pool file in C

--- data/main.c
+++ data/main.c
@@ -2,8 +2,26 @@
 #include <windows.h>
 #elif __linux__
 #include <unistd.h>
+#endif
+
 #include <stdio.h>
 
-void get_cpu_memory_usage() {
+void get_cpu_memory_usage()
+{
+#ifdef _WIN32
+    MEMORYSTATUSEX memInfo;
+    memInfo.dwLength = sizeof(MEMORYSTATUSEX);
+    GlobalMemoryStatusEx(&memInfo);
+    printf("Memory Usage: %ld%%\n", memInfo.dwMemoryLoad);
+    printf("Total Physical Memory: %lld MB\n", memInfo.ullTotalPhys / (1024 * 1024));
+    printf("Available Physical Memory: %lld MB\n", memInfo.ullAvailPhys / (1024 * 1024));
+#elif __linux__
+    // Add linux code here
+#endif
+}
 
+int main()
+{
+    get_cpu_memory_usage();
+    return 0;
 }
